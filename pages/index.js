0131d44a964e924b5e8fc233aed26ec224daa36e
Prateek Varma <prtk.varma@gmail.com>
2023-01-05 13:43:00 -0600

    Setup for the meetup app

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,12 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+  { id: 'm1', title: 'First meetup', image: 'https://images.pexels.com/photos/167385/pexels-photo-167385.jpeg?auto=compress&cs=tinysrgb&w=600', address: '515 Goodwill Rd, Round Island, AA', description: 'This is the first meetup' },
+  { id: 'm2', title: 'Second meetup', image: 'https://images.pexels.com/photos/258804/pexels-photo-258804.jpeg?auto=compress&cs=tinysrgb&w=600', address: '516 Goodwill Rd, Glittery Mountain, AA', description: 'This is the second meetup' }
+];
+
 function HomePage() {
-  return <h1>The Home Page</h1>
+  return <MeetupList meetups={DUMMY_MEETUPS} />
 }
 
 export default HomePage;
