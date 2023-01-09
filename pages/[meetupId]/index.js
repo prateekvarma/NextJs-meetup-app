a9502ca57b06ef03478380776ac2e566988f0486
Prateek Varma <prtk.varma@gmail.com>
2023-01-09 13:27:17 -0600

    Used getStaticPaths() and getStaticProps() to get data for dynamic paths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -4,4 +4,36 @@ function MeetupDetails() {
     return <MeetupDetail title="dummy title" address='dummy address' image="https://images.pexels.com/photos/69371/pexels-photo-69371.jpeg?auto=compress&cs=tinysrgb&w=600" description="Dummy desc" />
 }
 
+export async function getStaticPaths() {
+    return {
+        fallback: false,
+        paths: [
+            {
+                params: { meetupId: 'm1' }
+            },
+            {
+                params: { meetupId: 'm2' }
+            }
+        ]
+    }
+}
+
+export async function getStaticProps(context) {
+    //fetch data for a single meetup
+    const meetupId = context.params.meetupId;
+    console.log('Thy Id: ', meetupId);
+
+    return {
+        props: {
+        meetupData: {
+            id: meetupId,
+            title: "dummy title", 
+            address: "dummy address",
+            image: "https://images.pexels.com/photos/69371/pexels-photo-69371.jpeg?auto=compress&cs=tinysrgb&w=600",
+            description: "Dummy desc"
+            }
+        }
+    }
+}
+
 export default MeetupDetails;
