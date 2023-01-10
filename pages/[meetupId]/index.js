e3cae1fffd6755f07cc2a70bd8c9ffee51f29cc7
Prateek Varma <prtk.varma@gmail.com>
2023-01-10 13:53:50 -0600

    Generate dynamic URL based on the ID

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,20 +1,29 @@
+import { MongoClient } from "mongodb";
+
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
 function MeetupDetails() {
-    return <MeetupDetail title="dummy title" address='dummy address' image="https://images.pexels.com/photos/69371/pexels-photo-69371.jpeg?auto=compress&cs=tinysrgb&w=600" description="Dummy desc" />
+    return <MeetupDetail 
+                title="dummy title" 
+                address='dummy address' 
+                image="https://images.pexels.com/photos/69371/pexels-photo-69371.jpeg?auto=compress&cs=tinysrgb&w=600" 
+                description="Dummy desc" 
+            />
 }
 
 export async function getStaticPaths() {
+    const client = await MongoClient.connect('xxx-mongoUrl-xxx');
+    const db = client.db();
+    const meetupsCollection = db.collection('meetups');
+
+    const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray(); //only return the ids on eahc doc
     return {
         fallback: false,
-        paths: [
-            {
-                params: { meetupId: 'm1' }
-            },
-            {
-                params: { meetupId: 'm2' }
+        paths: meetups.map((meetup) => {
+            return {
+                params: { meetupId: meetup._id.toString() }
             }
-        ]
+        })
     }
 }
 
